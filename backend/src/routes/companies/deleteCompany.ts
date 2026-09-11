5b5db4aa13c07f81b28aa5a9cb57cee480ee6e8c
LucasLeonel-dev <lucleonelg4@gmail.com>
2026-09-11 15:39:17 -0300

    complete getCompanies + add deleteCompany

---
+++ backend/src/routes/companies/deleteCompany.ts
@@ -0,0 +1,16 @@
+import { request } from "node:http";
+import {prisma} from "../../lib/prisma.js";
+import { FastifyInstance } from "fastify";
+
+export default async function deleteCompany(app: FastifyInstance){
+    app.delete('/companies/:id', {preHandler: [app.authenticate]}, async(request, reply){
+        const {sub: userId} = request.user;
+        const {id} = request.params as {id: string};
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    })
+} 
--- backend/src/routes/companies/getCompanies.ts
+++ backend/src/routes/companies/getCompanies.ts
@@ -2,7 +2,7 @@ import {prisma} from "../../lib/prisma.js";
 import { FastifyInstance } from "fastify";
 
 export default async function getCompanies(app:FastifyInstance) {
-    app.get('/companies',{preHandler: [app.authenticate]} ,async(request, reply)=>{ //pre habdler verifica se o usario é autenticado
+    app.get('/companies',{preHandler: [app.authenticate]} ,async(request, reply)=>{ //pre handler verifica se o usario é autenticado
         const {sub: userId} = request.user;
 
         const companies = await prisma.company.findMany({
